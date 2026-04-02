3792ae19b9263bb6f182c41d17b7a231c0a95504
hiphuc179 <vthanhphucv1@gmail.com>
2026-04-02 20:34:02 +0700

    update controller structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'pages/login.html'),
+        adminDashboard: resolve(__dirname, 'pages/admin-dashboard.html'),
+        map: resolve(__dirname, 'pages/map.html'),
+        ownerDashboard: resolve(__dirname, 'pages/owner-dashboard.html'),
+        profile: resolve(__dirname, 'pages/profile.html'),
+        restaurant: resolve(__dirname, 'pages/restaurant.html')
+      }
+    }
+  }
 })
